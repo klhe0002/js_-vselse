7ef40dbb18214087b42157293d618687e3681977
klhe0002 <klhe0002@stud.kea.dk>
2025-01-29 20:08:22 +0100

    goddag

---
+++ goddag.js
@@ -0,0 +1,15 @@
+function hilsen() {
+  const tid = new Date().getHours();
+  if (tid < 5) {
+    console.log("Godnat");
+  } else if (tid < 10) {
+    console.log("Godmorgen");
+  } else if (tid < 18) {
+    console.log("Goddag");
+  } else {
+    console.log("Godaften");
+  }
+}
+window.onload = function () {
+  console.log(hilsen());
+};
